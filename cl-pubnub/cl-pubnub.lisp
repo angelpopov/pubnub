6535768b0f2a436450c42b5d62e84c720a709ac6
acer <apopov@yahoo.com>
2016-01-01 14:29:09 +0200

    added more urls

--- cl-pubnub/cl-pubnub.lisp
+++ cl-pubnub/cl-pubnub.lisp
@@ -4,32 +4,97 @@
 (defconstant +pub-key+ "demo")
 (defconstant +sub-key+ "demo")
 (defconstant +host+ "http://pubsub.pubnub.com")
+(defun to-url(&rest params)
+  (format nil "~{~A~^/~}" (list* +host+ params)))
 
 (defun peek-url (channel message)
-  (format nil "~{~A~^/~}" (list +host+ "publish" +pub-key+
-				0 ;; signature
-				channel
-				0 ;; callback
-				message)))
+  (to-url "publish" +pub-key+
+	  0 ;; signature
+	  channel
+	  0 ;; callback
+	  message))
+
+(defun time-url()(to-url "time" 0))
+
+(defun leave-url(channel uuid)
+  (to-url "v2" "presence" "sub_key" +sub-key+
+	  "channel" (do-urlencode:urlencode channel)
+	  "leave" (do-urlencode:urlencode (format nil "{'uuid':~S,'auth':''}" uuid))))
+(defun leave-group-url(channel uuid)
+  (to-url "v2" "presence" "sub_key" +sub-key+
+	  "channel" (do-urlencode:urlencode channel)
+	  "leave" (do-urlencode:urlencode (format nil "{'uuid':~S,'auth':''}" uuid))))
+
+(defun history-url(channel)
+  (to-url "v2" "history" "sub-key" +sub-key+ "channel"
+	  (do-urlencode:urlencode channel)
+	  ;;(do-urlencode:urlencode "{'string_message_token':'true'}") TBA if uncommented, it shows 0 items
+	  ) )
+(defun replay-url(source-channel dest-channel)
+  (to-url "v1" "replay" +pub-key+ +sub-key+
+	  (do-urlencode:urlencode source-channel)
+	  (do-urlencode:urlencode dest-channel)
+	  ;;(do-urlencode:urlencode "{'count':1}") TBA - result: not-found?!
+	  ))
+(defun here-now-url ()
+  (to-url "v2" "presence" "sub_key" +sub-key+) ;; TBA - should we restrict to max 100 items?
+  )
+
+(defun where-now-url (uuid)
+  (to-url "v2" "presence" "sub_key" +sub-key+ "uuid" uuid))
+
+(defun state-url (channel uuid)
+  (to-url "v2" "presence" "sub_key" +sub-key+ "channel" (do-urlencode:urlencode channel)
+	  "uuid" uuid))
+
+(defun grant-url(channel)
+  (to-url "v1" "auth" "sub_key" +sub-key+ (do-urlencode:urlencode "{
+                channel  : 'my_chat',
+                callback : fun,
+                error    : fun,
+                ttl      : 24 * 60, // Minutes
+                read     : true,
+                write    : true,
+                auth_key : '3y8uiajdklytowsj'
+            }") ))
+
+(defun mobile-gw-prowision-url (device-id
+			    op ;; add | remove
+			    gw-type ;; apns' | 'gcm'
+			    channel)
+  (to-url
+   "v1/push/sub-key" +sub-key+
+   "devices" device-id
+   (do-urlencode:urlencode (format nil "{
+         device_id: '~A',
+         op       : '~A',
+         gw_type  : '~A',
+         channel  : '~A',
+         callback : fun,
+         error    : fun,
+         }" device-id op gw-type channel ))))
+
+(defun audit-url ()
+  (to-url
+   "v1/auth/audit/sub-key" +sub-key+
+   (do-urlencode:urlencode (format nil "{
+         timestamp : 0}" ))))
 
 (defun publish-url (channel message)
-  (format nil "~{~A~^/~}" (list +host+ "publish" +pub-key+ +sub-key+
-				0 ;; signature
-				channel
-				0 ;; callback
-				(do-urlencode:urlencode message))))
+  (to-url "publish" +pub-key+ +sub-key+
+	  0 ;; signature
+	  channel
+	  0 ;; callback
+	  (do-urlencode:urlencode message)))
 
 (defun subscribe-url(channel timetoken)
-  (format nil "~{~A~^/~}" (list +host+ "subscribe" +sub-key+
-				channel
-				0 ;; callback
-				timetoken)))
+  (to-url "subscribe" +sub-key+
+	  channel
+	  0 ;; callback
+	  timetoken))
 
-(defun history-url(channel limit &key (callback 0))
-  (format nil "~{~A~^/~}" (list +host+ "history" +sub-key+
-				channel
-				callback
-				limit)))
+(defun history-old-url(channel limit &key (callback 0))
+  (to-url "history" +sub-key+	  channel	  callback	  limit))
 
 (defun pubnub (url) (jsown:parse (drakma:http-request url)) )
 
@@ -45,7 +110,7 @@
 		       obj))
 
 (defun history (channel &optional (limit 100))
-  (pubnub (history-url channel limit)))
+  (pubnub (history-url channel)))
 
 (defun pub-peek ()
   "looks like a bug in the api, but it is funny to peek what is going on."
